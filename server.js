1bb78eb5eac75597d295b79b515ff3bc67368a1a
Sergey Khokhlov <sergey.a.khokhlov@gmail.com>
2017-03-02 19:22:09 +0300

    Перенаправление на страницу безопасности

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get(
 
 app.get('/', (req, res) => {
     if (req.headers['cf-connecting-ip'] === '46.44.45.0' && req.cookies.safety !== 'true') {
-        res.redirect('/special/safety');
+        return res.redirect('/special/safety');
     }
     if (req.cookies.lang === 'en') {
         res.redirect('/en');
